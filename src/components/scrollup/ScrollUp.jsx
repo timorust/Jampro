c01a3412240ba9ddb62d915824b770b5b43062fa
tamir.rustamov <timorust1@gmail.com>
2024-10-31 21:29:55 +0200

    eslint correct

--- src/components/scrollup/ScrollUp.jsx
+++ src/components/scrollup/ScrollUp.jsx
@@ -1,17 +1,28 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './scrollup.css'
 
 const ScrollUp = () => {
-	window.addEventListener('scroll', function () {
-		const scrollUp = document.querySelector('.scrollup')
+	useEffect(() => {
+		const handleScroll = () => {
+			const scrollUp = document.querySelector('.scrollup')
+			if (window.scrollY >= 560) scrollUp.classList.add('show-scroll')
+			else scrollUp.classList.remove('show-scroll')
+		}
+		window.addEventListener('scroll', handleScroll)
+		return () => window.removeEventListener('scroll', handleScroll)
+	}, [])
 
-		if (this.scrollY >= 560) scrollUp.classList.add('show-scroll')
-		else scrollUp.classList.remove('show-scroll')
-	})
 	return (
-		<a href='#' className='scrollup'>
+		<button
+			className='scrollup'
+			onClick={e => {
+				e.preventDefault()
+				window.scrollTo({ top: 0, behavior: 'smooth' })
+			}}
+			aria-label='Scroll to top'
+		>
 			<i className='uil uil-arrow-up scrollup__icon'></i>
-		</a>
+		</button>
 	)
 }
 
--- src/components/work/WorkItems.jsx
+++ src/components/work/WorkItems.jsx
@@ -7,9 +7,23 @@ const WorkItems = ({ item }) => {
 
 			<h3 className='work__title'>{item.title}</h3>
 
-			<a href='#' className='work__button'>
-				Demo <i className='bx bx-right-arrow-alt work__button-icon'></i>
-			</a>
+			{item.link ? (
+				<a
+					href={item.link}
+					target='_blank'
+					rel='noopener noreferrer'
+					className='work__button'
+				>
+					Demo <i className='bx bx-right-arrow-alt work__button-icon'></i>
+				</a>
+			) : (
+				<button
+					className='work__button'
+					onClick={() => alert('Demo link not available')}
+				>
+					Demo <i className='bx bx-right-arrow-alt work__button-icon'></i>
+				</button>
+			)}
 		</div>
 	)
 }
